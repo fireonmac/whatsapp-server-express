07de9f14234202136f1ef75f262eabdda92251b4
Paul Noh <fireonmac@gmail.com>
2024-03-02 22:29:08 +0900

    feat: add morgan and winston logger
    Improves logging functionality by adding Morgan and Winston Logger.
    
    - Uses Morgan to log HTTP request information to the console.
    - Uses Winston Logger to log important messages and errors to a file.
    
    **Additional changes:**
    
    - Installed the `winston` package.
    
    References:
    
    - [Medium Article]
    (https://levelup.gitconnected.com/better-logs-for-expressjs-using-winston-and-morgan-with-typescript-1c31c1ab9342)
    - [Morgan](https://github.com/expressjs/morgan)
    - [Winston Logger](https://github.com/winstonjs/winston)

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
 import express from 'express';
-import morgan from 'morgan';
 import helmet from 'helmet';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
 import mongoSanitize from 'express-mongo-sanitize';
+import morganMiddleware from './middlewares/morgan.middleware';
+import logger from './configs/logger.config';
 
 const app = express();
 
-if (process.env.NODE_ENV !== 'production') {
-    app.use(morgan('dev'));
-}
+app.use(morganMiddleware);
 
 app.use(helmet());
+app.use(mongoSanitize());
+
+app.use(compression());
+app.use(cookieParser());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(mongoSanitize());
-app.use(cookieParser());
-app.use(compression());
 app.use(
     fileUpload({
         useTempFiles: true,
@@ -26,7 +27,7 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-    res.send(req.body);
+    logger.info(req.body);
 });
 
 export default app;
