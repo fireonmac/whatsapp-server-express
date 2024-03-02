447e6ced504b86d72924c0617193c78d01a94432
Paul Noh <fireonmac@gmail.com>
2024-03-02 18:12:43 +0900

    feat: add middlewares
    * Add `morgan('dev')` middleware to print logs in development environment.
    * Add `helmet()` middleware to enhance security.
    * Add `express.json()` middleware to parse JSON request data.
    * Add `express.urlencoded({ extended: true })` middleware to parse URL-encoded request data.
    * Add `mongoSanitize()` middleware to prevent MongoDB injection attacks.
    * Add `cookieParser()` middleware to parse cookies.
    * Add `compression()` middleware to compress response data.
    * Add `fileUpload()` middleware to add file upload functionality.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+import mongoSanitize from 'express-mongo-sanitize';
 
 const app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'));
+}
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(mongoSanitize());
+app.use(cookieParser());
+app.use(compression());
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: '/tmp/',
+    })
+);
+
 app.get('/', (req, res) => {
-    res.send('HELLO WORLD FROM THE SERVER');
+    res.send(req.body);
 });
 
 export default app;
